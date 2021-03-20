e59e0e406fd118fa2bc5316af601945237d557e0
bohachu <cbh@cameo.tw>
2021-03-20 16:48:48 +0800

    幾乎完成了

--- cameo-record-screen.js
+++ cameo-record-screen.js
@@ -1,35 +1,55 @@
-const start = document.getElementById("start");
-const stop = document.getElementById("stop");
-const video = document.querySelector("video");
-let recorder, stream;
+class CameoRecordScreen extends HTMLElement {
+  connectedCallback() {
+    this.build_gui();
 
-async function startRecording() {
-  stream = await navigator.mediaDevices.getDisplayMedia({
-    video: { mediaSource: "screen" }
-  });
-  recorder = new MediaRecorder(stream);
+    const start = document.getElementById("start");
+    const stop = document.getElementById("stop");
+    const video = document.querySelector("video");
+    let recorder, stream;
 
-  const chunks = [];
-  recorder.ondataavailable = (e) => chunks.push(e.data);
-  recorder.onstop = (e) => {
-    const completeBlob = new Blob(chunks, { type: chunks[0].type });
-    video.src = URL.createObjectURL(completeBlob);
-  };
+    async function startRecording() {
+      stream = await navigator.mediaDevices.getDisplayMedia({
+        video: {mediaSource: "screen"}
+      });
+      recorder = new MediaRecorder(stream);
 
-  recorder.start();
+      const chunks = [];
+      recorder.ondataavailable = (e) => chunks.push(e.data);
+      recorder.onstop = (e) => {
+        const completeBlob = new Blob(chunks, {type: chunks[0].type});
+        video.src = URL.createObjectURL(completeBlob);
+      };
+
+      recorder.start();
+    }
+
+    start.addEventListener("click", () => {
+      start.setAttribute("disabled", true);
+      stop.removeAttribute("disabled");
+
+      startRecording();
+    });
+
+    stop.addEventListener("click", () => {
+      stop.setAttribute("disabled", true);
+      start.removeAttribute("disabled");
+
+      recorder.stop();
+      stream.getVideoTracks()[0].stop();
+    });
+  }
+
+  build_gui() {
+    this.innerHTML = `
+      <button id="start">
+        Start Recording
+      </button>
+      <button id="stop" disabled>
+        Stop Recording
+      </button>
+      <video autoPlay/>
+    `;
+  }
 }
 
-start.addEventListener("click", () => {
-  start.setAttribute("disabled", true);
-  stop.removeAttribute("disabled");
-
-  startRecording();
-});
-
-stop.addEventListener("click", () => {
-  stop.setAttribute("disabled", true);
-  start.removeAttribute("disabled");
-
-  recorder.stop();
-  stream.getVideoTracks()[0].stop();
-});
+customElements.define("cameo-record-screen", CameoRecordScreen);
